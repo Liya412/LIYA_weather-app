97a1cfaa80846d081c79819b8cedff1e55171e23
Liya <liyazhang412@gmail.com>
2023-08-27 20:05:00 -0400

    Used the One Call API to retrieve forecast data
    Add functions that make API calls and update forecasts.
    Initial setup.

--- src/script.js
+++ src/script.js
@@ -14,11 +14,13 @@ function formatDate(timestamp) {
   const day = weekDays[date.getDay()];
   const hour = date.getHours();
   const min = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
-  return `${day} ${hour}:${min}`;
+  // return `${day} ${hour}:${min}`;
+  return { DD: day, HH: hour, mm: min };
 }
 
 // This is a function responsible for updating the current weather info and icon
 function updateOverview(response) {
+  // Assign the Celsius temperature values to the global variables
   cTempCurrent = response.data.main.temp;
   cTempMax = response.data.main.temp_max;
   cTempMin = response.data.main.temp_min;
@@ -27,8 +29,12 @@ function updateOverview(response) {
   const city = document.getElementById("city");
   city.innerText = response.data.name;
   const time = document.getElementById("time");
-  time.innerText = formatDate(response.data.dt);
-
+  time.innerText =
+    formatDate(response.data.dt).DD +
+    " " +
+    formatDate(response.data.dt).HH +
+    ":" +
+    formatDate(response.data.dt).mm;
   const weatherIcon = document.getElementById("weather-icon");
   weatherIcon.setAttribute(
     "src",
@@ -49,13 +55,19 @@ function updateOverview(response) {
   humidity.innerHTML = response.data.main.humidity;
   const windSpeed = document.getElementById("wind-speed");
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
+
+  // Store an object of coordinates in a variable
+  let coords = response.data.coord;
+  getForecast(coords);
 }
 
-// This is a funtion calling the realtime API from weatherapi.com
-function weatherApiCall(city) {
+// This funtion calls the realtime API from OpenWeather
+function getCurrentWeather(event) {
+  event.preventDefault();
+  const city = document.getElementById("city-input").value;
+
   const apiKey = "5aac6d0188c6f17d6d2bbe6591b6fef0";
-  const unit = "metric";
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
   axios
     .get(apiUrl)
@@ -65,14 +77,15 @@ function weatherApiCall(city) {
     });
 }
 
-weatherApiCall("tokyo");
+const searchForm = document.getElementById("search-form");
+searchForm.addEventListener("submit", getCurrentWeather);
 
-// This is a callback function calling the weather API from OpenWeather
+// This function calls the current weather API from OpenWeather
 function getWeatherData(event) {
   event.preventDefault();
   const userCity = document.getElementById("city-input").value;
 
-  const apiKey = "5aac6d0188c6f17d6d2bbe6591b6fef0";
+  const apiKey = "caa883a4a60d93878755b08a933f74ea";
   const unit = "metric";
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&appid=${apiKey}&units=${unit}`;
 
@@ -84,9 +97,6 @@ function getWeatherData(event) {
       console.log(error);
     });
 }
-
-const searchForm = document.getElementById("search-form");
-searchForm.addEventListener("submit", getWeatherData);
 
 // Feature 3
 // Add a current location button. Click it to get users' location and the temperature in it
@@ -100,9 +110,8 @@ function getPosition(event) {
 function success(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const unit = "metric";
   const apiKey = "5aac6d0188c6f17d6d2bbe6591b6fef0";
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 
   axios
     .get(apiUrl)
@@ -152,29 +161,58 @@ degreeFahr.addEventListener("click", celToFahr);
 let degreeCel = document.getElementById("degree-celsius");
 degreeCel.addEventListener("click", fahrToCel);
 
-// This function repeats a block of code five times
-function repeatForecast() {
-  const forecast = document.getElementById("weather-forecast");
-
-  const fiveDays = ["Mon", "Tue", "Wed", "Thu", "Fri"];
-
+// This function updates the weather forecast and repeats a block of code five times
+function updateForecast(response) {
+  const dailyForecast = response.data.daily;
   let forecastHTML = `<div class="row justify-content-center">`;
-  fiveDays.forEach((element) => {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2 forecast">
-          <div>${element}</div>
+
+  dailyForecast.forEach((element, index) => {
+    if (0 < index && index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2 forecast">
+          <div>${formatDate(element.dt).DD.slice(0, 3)}</div>
           <img
                 class="weather-icon"
-                src="https://basmilius.github.io/weather-icons/production/fill/all/cloudy.svg"
+                src= ${weatherIconUrls[element.weather[0].icon]}
                 alt="sunny"
                 width="70"
           />
-          <div>32°&nbsp;&nbsp;<span class="temp-min">28°</span></div>
+          <div>${Math.round(
+            element.temp.max
+          )}°&nbsp;&nbsp;<span class="temp-min">${Math.round(
+          element.temp.min
+        )}</span></div>
         </div>`;
+    }
   });
   forecastHTML = forecastHTML + "</div>";
+
+  const forecast = document.getElementById("weather-forecast");
   forecast.innerHTML = forecastHTML;
 }
 
-repeatForecast();
+// This funtion calls the One Call API from OpenWeather
+function getForecast(coordinates) {
+  console.log(coordinates);
+  const apiKey = "caa883a4a60d93878755b08a933f74ea";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios
+    .get(apiUrl)
+    .then(updateForecast)
+    .catch((error) => console.log(error));
+}
+
+// This function calls the current weather API for Shanghai and initializes the page
+function initialization() {
+  const apiKey = "caa883a4a60d93878755b08a933f74ea";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=shanghai&appid=${apiKey}&units=metric`;
+
+  axios
+    .get(apiUrl)
+    .then(updateOverview)
+    .catch((error) => console.log(error));
+}
+
+initialization();
